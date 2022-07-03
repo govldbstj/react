54f7bfcc0ad314d90212c887dea348616c495e87
Jay Choi <mobilechoi@gmail.com>
2022-07-03 22:18:29 +0900

    Align Card Elemtns to Center

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Typography, AppBar} from '@mui/material';
+import {Typography, AppBar, CssBaseline} from '@mui/material';
 import MusicList from './MusicList';
 import music_list from './data';
 
 export default function App () {
     return (
         <React.Fragment>
+            <CssBaseline/>
 
             <AppBar position="fixed">
                 <Typography align="center" variant="h3" color="inherit">Favorite Music</Typography>
             </AppBar>
-            
+
             <div style={{height: 60, width: '100%'}}></div>
             <MusicList list={music_list}>
             </MusicList>
--- src/MusicList.js
+++ src/MusicList.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import {Card, CardContent, Typography} from '@mui/material';
 
+const styles = {
+    content : {},
+    layout : {
+        display : 'flex',
+        justifyContent : 'center'
+    },
+    card: {
+        minWidth: 275,
+        maxWidth: 600,
+        marginBottom : "20pt",
+        marginLeft : 'auto',
+        marginRight : 'auto',
+    },
+};
+
 export default function MusicList ({list}) {
     return (
         <div>
             {list.results.map(item => {
                 return (
-                <Card>
+                <Card sx={styles.card} key={item.collectionId}>
                     <CardContent>
                         <Typography variant="subtitle1"> {item.artistName}</Typography>
                         <Typography variant="subtitle2"> {item.collectionCensoredName}</Typography>
